0644d26cec0584b8dd209e91eb6477897cd494e3
godzzolp <godzzo@lightport.hu>
2019-12-02 09:21:23 +0100

    PagedResourcesAssembler working without Jackson - StackOverflow

--- src/main/kotlin/org/mysql/sakila/controller/CityController.kt
+++ src/main/kotlin/org/mysql/sakila/controller/CityController.kt
@@ -1,5 +1,6 @@
 package org.mysql.sakila.controller
 
+import org.mysql.sakila.domain.City
 import org.mysql.sakila.repository.CityRepository
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
@@ -7,7 +8,10 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.rest.webmvc.PersistentEntityResource
 import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler
 import org.springframework.data.rest.webmvc.RepositoryRestController
+import org.springframework.data.web.PagedResourcesAssembler
 import org.springframework.hateoas.CollectionModel
+import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.PagedModel
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
@@ -25,8 +29,8 @@ class CityController(
         log.info("CityController INITIALIZED...");
     }
 
-    @RequestMapping(value = ["/api/cities/advanced"], method = arrayOf(RequestMethod.GET))
-    fun cities(pageable: Pageable,
+    @RequestMapping(value = ["/{repository}/extendCities"], method = arrayOf(RequestMethod.GET))
+    fun extendCities(pageable: Pageable,
                 assembler: PersistentEntityResourceAssembler) : ResponseEntity<CollectionModel<PersistentEntityResource>> {
 
         log.info("CityController CALLED!!!");
@@ -39,4 +43,20 @@ class CityController(
 
         return ResponseEntity(assembler.toCollectionModel(persons), HttpStatus.OK);
     }
+
+    @RequestMapping(value = ["/{repository}/extendCitiesPage"], method = arrayOf(RequestMethod.GET))
+    fun extendCitiesPage(pageable: Pageable,
+                         assembler: PagedResourcesAssembler<City>) : ResponseEntity<PagedModel<EntityModel<City>>> {
+
+        log.info("CityController - extendCitiesPage - CALLED!!!");
+
+        val persons = repository.findAll(pageable);
+
+        persons.forEach() {
+            log.info("row: ${it.id}");
+        }
+
+        return ResponseEntity(assembler.toModel(persons), HttpStatus.OK);
+    }
+
 }
